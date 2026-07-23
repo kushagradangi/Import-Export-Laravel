b964f41399c0702147a6dde75b3335c10516059c
kushagradangi <kushagradangi10@gmail.com>
2026-07-24 00:50:16 +0530

    step 10 blade view created

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/',[StudentController::class,'index']);
+Route::post('/import',[StudentController::class,'import']);
+
